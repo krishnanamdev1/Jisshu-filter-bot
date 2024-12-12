dca515949c09ac28ded8e4f1903a26b6e8214c9c
krishnanamdev1 <121806532+krishnanamdev1@users.noreply.github.com>
2024-12-12 21:15:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+API_ID = int(environ.get('API_ID', '26479212'))
+API_HASH = environ.get('API_HASH', '448ef3436e4d51fcded71c4429f6c3c8')
+BOT_TOKEN = environ.get('BOT_TOKEN', '8061311613:AAH2-X_st9lkZrPRuV_yOUobsrAcf7HsCKI')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5709508529').split()]
