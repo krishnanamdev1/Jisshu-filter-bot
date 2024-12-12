2ba2788c6dc6574a2eb25a46764f3e085b940b55
krishnanamdev1 <121806532+krishnanamdev1@users.noreply.github.com>
2024-12-12 21:30:02 +0530

    Update by me

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', "https://t.me/KN_DORAEMON") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002476916396'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+GV7NLC7XgUJmYmY1')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002375909947').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://c6288030:china@123@cluster0.b3slq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
